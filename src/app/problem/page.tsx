e30079cb295712bfa142faeb01e9498e658abf96
Bernardomennndes <bernardomennndes@outlook.com>
2023-11-27 22:14:21 -0300

    [ADD] Solution for "Rádio terapia" problem added

--- src/app/problem/page.tsx
+++ src/app/problem/page.tsx
@@ -1,32 +1,54 @@
-"use client"
+"use client";
 
-import ProblemTable from '@/components/ProblemTable'
-import { Button } from '@/components/ui/button'
-import { Flex } from '@radix-ui/themes'
-import Link from 'next/link'
-import { notFound, useSearchParams } from 'next/navigation'
-import React from 'react'
+import ProblemTable from "@/components/ProblemTable";
+import { Button } from "@/components/ui/button";
+import { Flex } from "@radix-ui/themes";
+import Link from "next/link";
+import { notFound, useSearchParams } from "next/navigation";
+import React from "react";
 
-export default function Problem({ teste } : {teste : string}){
-  const searchParams = useSearchParams()
+export default function Problem({ test }: Readonly<{ test: string }>) {
+  const searchParams = useSearchParams();
 
-  let numRestrictions : number, numVariables : number
+  let numRestrictions: number, numVariables: number;
 
-  try{
-    numRestrictions = Number.parseInt(searchParams.get("res").toString())
-    numVariables = Number.parseInt(searchParams.get("var").toString())
+  try {
+    numRestrictions = Number.parseInt(
+      searchParams?.get("res")?.toString() ?? ""
+    );
+    numVariables = Number.parseInt(searchParams?.get("var")?.toString() ?? "");
+  } catch {
+    return notFound();
   }
-  catch{
-    return notFound()
-  }
 
-  if(numRestrictions < 2 || numRestrictions> 10 || numVariables > 10 || numVariables < 2) return notFound()
+  if (
+    numRestrictions < 2 ||
+    numRestrictions > 10 ||
+    numVariables > 10 ||
+    numVariables < 2
+  )
+    return notFound();
+
+  async function handleCallApi() {
+    try {
+      const response = await fetch("/api/simplex", { method: "GET" });
+
+      console.log(await response.json());
+    } catch {
+      console.log("Api call failed.");
+    }
+  }
 
   return (
     <Flex align="center" justify="center" direction="column" gap="5">
-      <ProblemTable numRestrictions={numRestrictions} numVariables={numVariables}/>
-      <Button>Resolver</Button>
-      <Button variant="link"><Link href="/">Voltar</Link></Button>
+      <ProblemTable
+        numRestrictions={numRestrictions}
+        numVariables={numVariables}
+      />
+      <Button onClick={() => handleCallApi()}>Resolver</Button>
+      <Button variant="link">
+        <Link href="/">Voltar</Link>
+      </Button>
     </Flex>
-  )
+  );
 }
--- src/pages/api/simplex/index.ts
+++ src/pages/api/simplex/index.ts
@@ -1,57 +1,70 @@
 const GLPK = require("glpk.js");
+import type { GLPK as GLPKType, Result, Options } from "glpk.js";
+
 import type { NextApiRequest, NextApiResponse } from "next";
 
-type ResponseData = {};
+type ResponseData = {
+  solution: Result;
+  iterations: Result[];
+};
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
-  const glpk = GLPK();
+  const glpk = GLPK() as GLPKType;
 
-  const options = {
+  const iterations: Result[] = [];
+
+  const options: Options = {
     msglev: glpk.GLP_MSG_ALL,
-    presol: true,
+    presol: false,
     cb: {
-      call: (progress: any) => console.log(progress),
+      call: (iteration) => iterations.push(iteration),
       each: 1,
     },
   };
 
   const response = glpk.solve(
     {
-      name: "LP",
+      name: "RT",
       objective: {
-        direction: glpk.GLP_MAX,
-        name: "obj",
+        direction: glpk.GLP_MIN,
+        name: "Z",
         vars: [
-          { name: "x1", coef: 0.6 },
+          { name: "x1", coef: 0.4 },
           { name: "x2", coef: 0.5 },
         ],
       },
       subjectTo: [
         {
-          name: "cons1",
+          name: "constraint-1",
           vars: [
-            { name: "x1", coef: 1.0 },
-            { name: "x2", coef: 2.0 },
+            { name: "x1", coef: 0.6 },
+            { name: "x2", coef: 0.4 },
           ],
-          bnds: { type: glpk.GLP_UP, ub: 1.0, lb: 0.0 },
+          bnds: { type: glpk.GLP_LO, ub: 0.0, lb: 6.0 },
         },
         {
-          name: "cons2",
+          name: "constraint-2",
           vars: [
-            { name: "x1", coef: 3.0 },
-            { name: "x2", coef: 1.0 },
+            { name: "x1", coef: 0.5 },
+            { name: "x2", coef: 0.5 },
           ],
-          bnds: { type: glpk.GLP_UP, ub: 2.0, lb: 0.0 },
+          bnds: { type: glpk.GLP_FX, ub: 6.0, lb: 6.0 },
+        },
+        {
+          name: "constraint-3",
+          vars: [
+            { name: "x1", coef: 0.3 },
+            { name: "x2", coef: 0.1 },
+          ],
+          bnds: { type: glpk.GLP_UP, ub: 2.7, lb: 0.0 },
         },
       ],
     },
     options
   );
 
-  console.log(response);
-
-  return res.status(200).json({});
+  return res.status(200).json({ solution: response, iterations });
 }
